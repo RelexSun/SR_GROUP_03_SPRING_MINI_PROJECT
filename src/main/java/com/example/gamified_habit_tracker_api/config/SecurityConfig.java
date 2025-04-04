253bbb39511f36f236661a2b79d4bf9cc2c0cd0a
RelexSun <88622289+RelexSun@users.noreply.github.com>
2025-04-04 19:48:12 +0700

    permit all swagger

---
+++ src/main/java/com/example/gamified_habit_tracker_api/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.gamified_habit_tracker_api.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults()).csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("api/v1/auths/**", "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
