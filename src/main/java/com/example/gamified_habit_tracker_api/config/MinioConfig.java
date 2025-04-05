1a9dad1c72c7a09eb3280a5892db2f90a25d71e2
Zen-k3i <kimhengkou1604@gmail.com>
2025-04-05 17:35:24 +0700

    minio complete

---
+++ src/main/java/com/example/gamified_habit_tracker_api/config/MinioConfig.java
@@ -0,0 +1,28 @@
+package com.example.gamified_habit_tracker_api.config;
+
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MinioConfig {
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.access.name}")
+    private String accessKey;
+
+    @Value("${minio.access.secret}")
+    private String accessSecret;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, accessSecret)
+                .build();
+    }
+}
